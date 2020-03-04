042ad5b6fccfad35f191baede4748d980c80c698
unknown <anupamprataps@yahoo.com>
2020-03-04 16:36:38 +0800

    new file:   currency_rate.py

---
+++ currency_rate.py
@@ -0,0 +1,19 @@
+import urllib
+import pandas
+import numpy
+
+url = 'https://www.iban.com/exchange-rates'
+print("Opening the file connection...")
+uh= urllib.request.urlopen(url)
+print("HTTP status",uh.getcode())
+html =uh.read().decode()
+soup=bs.BeautifulSoup(html,'html.parser')
+currency=[]
+for tag in soup.find_all('td'):
+    currency.append(tag.text)
+currency=np.array(currency);
+currency=np.split(currency,len(currency)/4);
+currency=pd.DataFrame(currency,columns=['a', 'b','c','d'])
+currency=currency.drop(columns=['d']);
+currency['a']=currency['a'].str.replace('\t','');
+print(currency)
